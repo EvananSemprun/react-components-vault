a63a96301342d859e6e15222ef5436cbf30898bd
EvananSemprun <evanansemprun@gmail.com>
2023-07-17 12:37:13 -0400

    Buttons 4

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NotFound from './components/NotFound';
 import Button1 from './components/pages/Button_1'
 import Button2 from './components/pages/Button_2';
 import Button3 from './components/pages/Button_3';
+import Button4 from './components/pages/Button_4';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/Button1" element={<Button1/>} />
         <Route path="/Button2" element={<Button2/>} />
         <Route path="/Button3" element={<Button3/>} />
+        <Route path="/Button4" element={<Button4/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/pages/Button_4.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button_4.scss';
+
+const Button = () => {
+  return (
+    <button className="fill-in-button">hola</button>
+  );
+};
+
+export default Button;
