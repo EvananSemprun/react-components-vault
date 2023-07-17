ad8d76056ec43c885a5b514005ea65c72ee10c81
EvananSemprun <evanansemprun@gmail.com>
2023-07-17 12:07:19 -0400

    Buttons 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from './components/Home';
 import NotFound from './components/NotFound';
 import Button1 from './components/pages/Button_1'
 import Button2 from './components/pages/Button_2';
+import Button3 from './components/pages/Button_3';
+
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Button1" element={<Button1/>} />
         <Route path="/Button2" element={<Button2/>} />
+        <Route path="/Button3" element={<Button3/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
