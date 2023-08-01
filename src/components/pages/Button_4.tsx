364dbddb0a7024d8a704701b785708db817a6b04
EvananSemprun <evanansemprun@gmail.com>
2023-08-01 15:15:05 -0400

    button 4

--- src/components/pages/Button_4.tsx
+++ src/components/pages/Button_4.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import './Button_4.scss';
-
+import './com/Button_4.scss';
+import { Group } from '@mantine/core';
 const Button = () => {
   return (
-    <button className="fill-in-button">hola</button>
+    <>
+      <Group spacing="xl">
+      <button className="fill-1">¡Hola!</button>
+
+      <button className="fill-2">Clic aquí</button>
+
+      <button className="fill-3">Presiona</button>
+
+      <button className="fill-4">Aceptar</button>
+
+      <button className="fill-5">Enviar</button>
+
+      <button className="fill-6">Comprar</button>
+
+      <button className="fill-7">Ver más</button>
+      
+      <button className="fill-8">Aceptar</button>
+
+      <button className="fill-9">Enviar</button>
+
+      <button className="fill-10">Aceptar</button>
+
+      </Group>
+
+    </>
+
   );
 };
 
