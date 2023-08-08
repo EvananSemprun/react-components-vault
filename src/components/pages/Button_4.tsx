e3244197494971643e15d363c08e952e750325f3
EvananSemprun <evanansemprun@gmail.com>
2023-08-08 15:01:29 -0400

    boton 4

--- src/components/pages/Button_4.tsx
+++ src/components/pages/Button_4.tsx
@@ -1,10 +1,10 @@
 import './com/Button_4.scss';
-import { Group ,Title} from '@mantine/core';
+import { Group, Title } from '@mantine/core';
 const Button = () => {
   return (
     <>
-    <Title align='center' order={1}>Hover Effects #2</Title>
-    
+      <Title align='center' order={1}>Hover Effects #2</Title>
+
       <Group spacing="xl" mt={150}>
         <button className="fill-1">¡Hola!</button>
 
@@ -20,13 +20,16 @@ const Button = () => {
 
         <button className="fill-8">Aceptar</button>
 
-      </Group>
-
-      <Group spacing="xl" mt={15}>
+        <button className="fill-10">Aceptar</button>
         <button className="fill-11">¡Clickea aquí!</button>
 
         <button className="fill-12">Siguiente</button>
 
+
+
+      </Group>
+
+      <Group spacing="xl" mt={15}>
 
 
       </Group>
@@ -49,25 +52,17 @@ const Button = () => {
         <button className="fill-18">Enviar</button>
         <button className="fill-19">Enviar</button>
         <button className="fill-20">Enviar</button>
-
-
-      </Group>
-
-      <Group spacing="xl" mt={15}>
-        <button className="fill-10">Aceptar</button>
+        <button className="fill-29">Enviar</button>
 
       </Group>
 
       <Group spacing="xl" mt={15}>
+
         <button className="fill-3">Presiona</button>
-
         <button className="fill-22">Presiona</button>
         <button className="fill-23">Presiona</button>
         <button className="fill-24">Presiona</button>
-
-
         <button className="fill-25">Presiona</button>
-
         <button className="fill-26">Presiona</button>
         <button className="fill-27">Presiona</button>
         <button className="fill-28">Presiona</button>
