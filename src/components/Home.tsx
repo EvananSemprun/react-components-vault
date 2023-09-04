f4433517e4f3081497cd073a331593c1d590af4c
EvananSemprun <evanansemprun@gmail.com>
2023-09-04 14:09:14 -0400

    animations

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import { useState, ChangeEvent } from 'react';
 import { Card, Title, Text, Button, Grid, Input } from '@mantine/core';
+import { motion } from 'framer-motion'; // Importa motion desde Framer Motion
 import co from './co.json';
 
 function Home() {
@@ -28,32 +29,41 @@ function Home() {
 
       <Grid gutter="lg" style={{ justifyContent: 'center' }}>
         {filteredData.map((item, index) => (
-          <Card
+          <motion.div
             key={index}
-            mt={15}
-            shadow="xl"
-            padding="lg"
-            ml={15}
-            mr={15}
+            initial={{ opacity: 0, scale: 0.5 }} // Animación inicial
+            animate={{ opacity: 1, scale: 1 }} // Animación al cargar
+            whileHover={{ scale: 1.2 }} // Animación al hacer hover
+            transition={{ duration: 0.5 }} // Duración de la animación
             style={{ minWidth: '250px', maxWidth: '400px' }}
           >
-            <img
-              src={item.imagen}
-              alt={item.nombre}
-              style={{
-                marginBottom: '1rem',
-                width: '100%',
-                height: 'auto',
-                objectFit: 'cover',
-              }}
-            />
-            <Text variant="h4" style={{ marginBottom: '0.5rem' }}>
-              {item.nombre}
-            </Text>
-            <Button component="a" href={item.ruta} fullWidth>
-              Ver componente
-            </Button>
-          </Card>
+            <Card
+              mt={25}
+              shadow="xl"
+              padding="lg"
+              mb={2}
+              ml={15}
+              mr={15}
+              style={{ width: '85%'}}
+            >
+              <img
+                src={item.imagen}
+                alt={item.nombre}
+                style={{
+                  marginBottom: '1rem',
+                  width: '100%',
+                  height: 'auto',
+                  objectFit: 'cover',
+                }}
+              />
+              <Text variant="h4" style={{ marginBottom: '0.5rem' }}>
+                {item.nombre}
+              </Text>
+              <Button component="a" href={item.ruta} fullWidth>
+                Ver componente
+              </Button>
+            </Card>
+          </motion.div>
         ))}
       </Grid>
     </>
